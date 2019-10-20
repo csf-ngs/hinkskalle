3227c331f8ffdca861f313799e6a1028d251f4b7
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2019-10-20 02:09:46 +0200

    fake non-persisted fsk user with minimal interface

--- backend/Hinkskalle/fsk_api.py
+++ backend/Hinkskalle/fsk_api.py
@@ -2,5 +2,13 @@ from flask import current_app
 
 from forskalle_api import FskApi
 
+class FskUser():
+  def __init__(self, username, is_admin):
+    self.username=username
+    self.is_admin=is_admin
+
 class HinkskalleFskApi(FskApi):
-  pass
+  @staticmethod
+  def sync_scientist(fsk_user_json):
+    is_admin = any([ group['name']=="Admin" for group in fsk_user_json['groups'] ])
+    return FskUser(fsk_user_json['username'], is_admin)

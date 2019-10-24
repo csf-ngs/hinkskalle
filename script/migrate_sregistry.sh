3c936a37484c5ea3008841dcc441cabd9eba88f6
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2019-10-24 15:56:05 +0200

    push old sregistry containers

---
+++ script/migrate_sregistry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /groups/vbcf-ngs/misc/infra/sregistry/pipeline
+
+ls | grep -v latest | while read img; do
+  IFS=$'\uf022'
+  read -ra IMG <<< "$img"
+  tag=${IMG[1]%@*}
+  singularity push -U "$img" library:///pipeline/${IMG[0]}:$tag
+done
+

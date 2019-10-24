83d7717be36a9252b9f3fe27ec15c1f72b6ecd44
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2019-10-24 21:39:21 +0200

    set default entity when pushing old images

--- script/migrate_sregistry.sh
+++ script/migrate_sregistry.sh
@@ -6,6 +6,6 @@ ls | grep -v latest | while read img; do
   IFS=$'\uf022'
   read -ra IMG <<< "$img"
   tag=${IMG[1]%@*}
-  singularity push -U "$img" library:///pipeline/${IMG[0]}:$tag
+  singularity push -U "$img" library://default/pipeline/${IMG[0]}:$tag
 done
 

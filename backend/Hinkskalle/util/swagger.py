8a892131bd12046a2fb0e3db7a06359fffe062a8
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2019-11-27 14:53:15 +0100

    fix swagger: add custom authenticator

--- backend/Hinkskalle/util/swagger.py
+++ backend/Hinkskalle/util/swagger.py
@@ -1,5 +1,19 @@
 from flask_rebar import SwaggerV2Generator
 from fsk_authenticator.swagger import register_fsk_authenticator
+from Hinkskalle.util.auth import FskOptionalAuthenticator
 
 generator = SwaggerV2Generator()
 register_fsk_authenticator(generator)
+
+def convert_authenticator(authenticator):
+  return (authenticator.name, {
+    'name': authenticator.name,
+    'type': 'apiKey',
+    'in': 'header',
+  })
+
+generator.register_authenticator_converter(
+    authenticator_class=FskOptionalAuthenticator,
+    converter=convert_authenticator,
+)
+

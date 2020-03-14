4d981ee49582288fce3323a407eec9a12e549068
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-03-14 19:52:56 +0100

    password auth endpoint

--- backend/Hinkskalle/routes/auth.py
+++ backend/Hinkskalle/routes/auth.py
@@ -1,11 +1,20 @@
-from Hinkskalle import registry, authenticator
+from Hinkskalle import registry, authenticator, rebar, db
+from Hinkskalle.models import TokenSchema, User, Token
 from Hinkskalle.util.auth import Scopes
-from flask_rebar import RequestSchema, ResponseSchema
+from flask_rebar import RequestSchema, ResponseSchema, errors
 from marshmallow import fields, Schema
-from flask import current_app
+from flask import current_app, g
+import secrets
 
 class TokenResponseSchema(ResponseSchema):
   status = fields.String()
+
+class GetTokenRequestSchema(RequestSchema):
+  username = fields.String(required=True)
+  password = fields.String(required=True)
+
+class GetTokenResponseSchema(ResponseSchema):
+  data = fields.Nested(TokenSchema)
 
 @registry.handles(
   rule='/v1/token-status',
@@ -17,3 +26,29 @@ def token_status():
   return {
     'status': 'welcome'
   }
+
+@registry.handles(
+  rule='/v1/get-token',
+  method='POST',
+  request_body_schema=GetTokenRequestSchema(),
+  response_body_schema=GetTokenResponseSchema(),
+)
+def get_token():
+  body = rebar.validated_body
+  try:
+    user = User.query.filter(User.username==body['username']).one()
+  except:
+    raise errors.Unauthorized("Username not found.")
+
+  if not user.is_active:
+    raise errors.Unauthorized("User is disabled.")
+  
+  if not user.check_password(body['password']):
+    raise errors.Unauthorized("Invalid password.")
+
+  g.authenticated_user = user
+  token = Token(id=secrets.token_urlsafe(48), user=user)
+  db.session.add(token)
+  db.session.commit()
+  return { 'data': token }
+

9772c13df24d99e9e4e9939ddb9700560841f75a
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-05 20:35:16 +0100

    vuex store for auth + tests

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,14 +1,93 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios, {AxiosInstance} from 'axios';
+
 Vue.use(Vuex)
 
+export interface User {
+  username: string,
+  email: string,
+  fullname: string,
+  role: "admin" | "user",
+  extra_data: any,
+}
+
+export interface SnackbarState {
+  show: boolean,
+  msg: string | null,
+}
+
+interface State {
+  backend: AxiosInstance,
+  snackbar: SnackbarState,
+  authToken: string,
+  authStatus: string,
+  currentUser: User | null,
+}
+
+const state: State = {
+  backend: axios.create({ baseURL: process.env.VUE_APP_BACKEND_URL }),
+  snackbar: {
+    show: false,
+    msg: '',
+  },
+  authToken: '',
+  authStatus: '',
+  currentUser: null,
+}
+
 export default new Vuex.Store({
-  state: {
+  state: state,
+  getters: {
+    authStatus: state => state.authStatus,
+    isLoggedIn: state => state.currentUser !== null,
   },
   mutations: {
+    openSnackbar(state: State, msg: string) {
+      state.snackbar.msg = msg;
+      state.snackbar.show = true;
+    },
+    closeSnackbar(state: State) {
+      state.snackbar.msg = '';
+      state.snackbar.show = false;
+    },
+    authRequested(state: State) {
+      state.authStatus = 'loading';
+    },
+    authSuccess(state: State, params: { token: string, user: User }) {
+      state.authToken = params.token;
+      state.currentUser = params.user;
+      state.authStatus = 'success';
+      state.backend.defaults.headers.common['Authorization']=`Bearer ${state.authToken}`;
+    },
+    authFailed(state: State, error) {
+      console.log(error);
+      state.authStatus = 'failed';
+      state.currentUser = null;
+      state.authToken = '';
+      delete state.backend.defaults.headers.common['Authorization'];
+    },
   },
   actions: {
+    requestAuth: ({state, commit, dispatch}, user: {username: string, password: string}) => {
+      return new Promise((resolve, reject) => {
+        commit('authRequested');
+        state.backend.post('/api/v1/get-token', { username: user.username, password: user.password })
+          .then(response => {
+            const token = response.data.token;
+            localStorage.setItem('token', token);
+            const user = response.data.user as User;
+            commit('authSuccess', { token, user });
+            resolve(response);
+          })
+          .catch(err => {
+            commit('authFailed', err);
+            localStorage.removeItem('token');
+            reject(err);
+          });
+      });
+    },
   },
   modules: {
   }
---
+++ frontend/tests/unit/store.spec.ts
@@ -0,0 +1,99 @@
+
+import store from '@/store';
+import { User } from '@/store';
+
+import axios from 'axios';
+
+jest.mock('axios');
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+store.state.backend = mockAxios;
+
+const testUser: User = {
+      username: 'test.hase',
+      email: 'test@ha.se',
+      fullname: 'Test Hase',
+      role: 'user',
+      extra_data: {},
+    };
+
+describe('store getters', () => {
+  it('has isLoggedIn getter', () => {
+    expect(store.getters.isLoggedIn).toBe(false);
+    store.state.currentUser = testUser;
+    expect(store.getters.isLoggedIn).toBe(true);
+  });
+});
+
+describe('store mutations', () => {
+  it('has snackbar mutations', () => {
+    store.commit('openSnackbar', 'Oink!');
+    expect(store.state.snackbar.msg).toBe('Oink!');
+    expect(store.state.snackbar.show).toBe(true);
+
+    store.commit('closeSnackbar');
+    expect(store.state.snackbar.msg).toBe('');
+    expect(store.state.snackbar.show).toBe(false);
+  });
+
+  it('has authRequested mutation', () => {
+    store.state.authStatus='';
+    store.commit('authRequested');
+    expect(store.state.authStatus).toBe('loading');
+  });
+
+  it('has authSuccess mutation', () => {
+    store.state.authToken = '';
+    store.state.authStatus = '';
+    store.state.currentUser = null;
+
+    store.commit('authSuccess', { token: 'supersecret', user: testUser });
+    expect(store.state.authStatus).toBe('success');
+    expect(store.state.authToken).toBe('supersecret');
+    expect(store.state.currentUser).toBe(testUser);
+    expect(store.state.backend.defaults.headers.common['Authorization']).toBe(`Bearer supersecret`);
+  });
+
+  it('has authFailed mutation', () => {
+    store.state.authStatus = '';
+    store.state.currentUser = testUser;
+    store.state.authToken = 'oink';
+    store.state.backend.defaults.headers.common['Authorization']='oink';
+
+    store.commit('authFailed', {});
+    expect(store.state.authStatus).toBe('failed');
+    expect(store.state.currentUser).toBeNull();
+    expect(store.state.authToken).toBe('');
+    expect(store.state.backend.defaults.headers.common).not.toHaveProperty('Authorization');
+    
+  });
+});
+
+describe('store actions', () => {
+  it('has requestAuth', () => {
+    mockAxios.post.mockResolvedValue({
+      data: { 
+        token: 'superoink',
+        user: testUser,
+      }
+    });
+    const postData = { username: 'test.hase', password: 'supersecret'};
+    const promise = store.dispatch('requestAuth', postData);
+    expect(mockAxios.post).toBeCalledWith('/api/v1/get-token', postData);
+    expect(store.state.authStatus).toBe('loading');
+    promise.then(() => {
+      expect(store.state.authStatus).toBe('success');
+      expect(store.state.authToken).toBe('superoink');
+      expect(store.state.currentUser).toBe(testUser);
+    });
+
+    mockAxios.post.mockRejectedValue({ fail: 'fail' });
+    const failPromise = store.dispatch('requestAuth', postData);
+    failPromise.catch(err => {
+      expect(store.state.authStatus).toBe('failed');
+      expect(store.state.currentUser).toBeNull();
+    });
+
+  });
+
+});

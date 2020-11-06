5fe5accff88b209e0a5a2a32d94a61317e94611a
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-06 11:34:33 +0100

    auth guard + test

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, { RouteConfig, NavigationGuard } from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store';
 
 Vue.use(VueRouter)
+
+export const isAuthenticated: NavigationGuard = (to, _, next) => {
+  if (!store.getters.isLoggedIn) {
+    return next({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    });
+  }
+  return next();
+};
 
 const routes: Array<RouteConfig> = [
   {
@@ -10,20 +21,19 @@ const routes: Array<RouteConfig> = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    return isAuthenticated(to, from, next);
+  }
+  next();
+});
 
 export default router
---
+++ frontend/tests/unit/router.spec.ts
@@ -0,0 +1,31 @@
+import router, { isAuthenticated } from '@/router';
+import store, { User } from '@/store';
+import { Route } from 'vue-router';
+
+describe('AuthGuard', () => {
+  it('redirects when not logged in', () => {
+    store.state.currentUser = null;
+    const nextFn = jest.fn();
+    const to = { fullPath: '/oink' } as Route;
+    const from = {} as Route;
+    isAuthenticated(to, from, nextFn);
+    expect(nextFn).toHaveBeenCalledTimes(1);
+    expect(nextFn).toHaveBeenCalledWith({ path: '/login', query: { redirect: to.fullPath }});
+  });
+
+  it('continues when logged in', () => {
+    store.state.currentUser = { username: 'test.hase' } as User;
+    const nextFn = jest.fn();
+    const to = {} as Route;
+    const from = {} as Route;
+    isAuthenticated(to, from, nextFn);
+    expect(nextFn).toHaveBeenCalledTimes(1);
+    expect(nextFn).toHaveBeenCalledWith();
+  });
+
+  it('uses guard', () => {
+    router.push('/').then(next => {
+      // XXX
+    });
+  });
+});

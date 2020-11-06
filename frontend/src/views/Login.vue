7d167f9366de34b99236db9f30e1cf09a05cef20
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-06 21:44:49 +0100

    login form validation + error handling

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,11 +10,14 @@
             <v-card-subtitle>
               (use your Forskalle account)
             </v-card-subtitle>
-            <v-form>
-              <v-text-field v-model="state.user.username" prepend-icon="mdi-account" name="Username" label="Username" required></v-text-field>
-              <v-text-field v-model="state.user.password" prepend-icon="mdi-lock" name="Password" label="Password" type="password" required></v-text-field>
+            <v-form v-model="state.canSubmit" @submit.prevent="doLogin()">
+              <v-text-field id="username" v-model="state.user.username" :rules="rules" prepend-icon="mdi-account" name="Username" label="Username" required></v-text-field>
+              <v-text-field id="password" v-model="state.user.password" :rules="rules" prepend-icon="mdi-lock" name="Password" label="Password" type="password" required></v-text-field>
+              <v-alert v-if="state.loginError" type="error" text>
+                {{state.loginError}}
+              </v-alert>
               <v-card-actions>
-                <v-btn id="login" primary large block @click="doLogin()" :disabled="!canSubmit">Login</v-btn>
+                <v-btn type="submit" id="login" primary large block :loading="loading" :disabled="!state.canSubmit">Login</v-btn>
               </v-card-actions>
             </v-form>
           </v-card>
@@ -26,15 +29,20 @@
 <script lang="ts">
 import Vue from 'vue';
 
+import { AxiosError } from 'axios';
+
 interface LocalState {
   user: {
     username: string;
     password: string;
   };
+  canSubmit: boolean;
+  loginError: string;
 }
 
 interface Data {
   state: LocalState;
+  rules: Array<(string) => boolean | string>;
 }
 
 export default Vue.extend({
@@ -44,25 +52,37 @@ export default Vue.extend({
       user: {
         username: '',
         password: '',
-      }
-    }
+      },
+      canSubmit: false,
+      loginError: '',
+    },
+    rules: [
+      (v: string): boolean | string => !!v || 'Required!',
+    ],
   }),
   methods: {
     doLogin() {
-      console.log(this.state);
       this.$store.dispatch('requestAuth', this.state.user)
         .then((res) => {
-          console.log(res);
+          this.$router.push('/');
         })
-        .catch((err) => {
-          console.error(err);
+        .catch((err: AxiosError) => {
+          if (err.response) {
+            this.state.loginError = err.response.data.message;
+          }
+          else if (err.request) {
+            this.state.loginError = "Unable to reach API";
+          }
+          else {
+            this.state.loginError = err.message;
+          }
         });
     }
   },
   computed: {
-    canSubmit(): boolean {
-      return this.state.user.username !== '' && this.state.user.password !== '';
-    }
+    loading(): boolean {
+      return this.$store.getters.authStatus === 'loading';
+    },
   }
 });
 </script>
--- frontend/tests/unit/login.spec.ts
+++ frontend/tests/unit/login.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
@@ -27,19 +27,55 @@ describe('Login.vue', () => {
     const wrapper = mount(Login, { localVue, store, vuetify, router });
     expect(wrapper.find('button#login').attributes('disabled')).toBeTruthy();
 
-    await wrapper.setData({
-      state: {
-        user: {
-          username: 'oink',
-          password: 'hase',
-        }
-      }
-    });
+    await wrapper.find('input#username').setValue('oink');
+    await wrapper.find('input#password').setValue('hase');
 
-    expect(wrapper.find('button#login').attributes('disabled')).toBeFalsy();
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.find('button#login').attributes('disabled')).toBeFalsy();
+    });
 
   });
 
+  it('routes on successful login', async () => {
+    const myLocal = createLocalVue();
+    myLocal.use(Vuex);
+
+    const mockRouter = { push: jest.fn() };
+    store = new Vuex.Store({
+      actions: {
+        requestAuth: jest.fn().mockResolvedValue('dummy'),
+      }
+    })
+    const wrapper = mount(Login, { localVue: myLocal, store, vuetify, 
+      mocks: { $router: mockRouter },
+      data: () => ({ state: { user: { username: 'oink', password: 'hase' }}})
+    });
+    
+    await wrapper.find('form').trigger('submit');
+    wrapper.vm.$nextTick(() => {
+      expect(mockRouter.push).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('shows error on failed login', async done => {
+    store = new Vuex.Store({
+      actions: {
+        requestAuth: jest.fn().mockRejectedValue({ message: 'dummy' }),
+      }
+    });
+    const wrapper = mount(Login, { localVue, store, vuetify, router });
+    await wrapper.find('form').trigger('submit');
+    // not really elegant
+    wrapper.vm.$nextTick(() => {
+      wrapper.vm.$nextTick(() => {
+        wrapper.vm.$nextTick(() => {
+          expect(wrapper.find('div.v-alert').text()).toContain('dummy');
+          done();
+        });
+      });
+    });
+
+  })
 
 
 });

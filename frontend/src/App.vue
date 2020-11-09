4cd822b071c69627d10262a29642c53540de12c6
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-09 21:24:46 +0100

    change to sidebar navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Hinkskalle</span>
-      </v-toolbar-title>
-      <v-toolbar-items>
-        <v-btn text @click="$router.push('/')">Home</v-btn>
-      </v-toolbar-items>
-      <v-spacer></v-spacer>
-      <v-btn v-if="isLoggedIn" text>
-        {{currentUser.fullname}}
-        <v-icon right>mdi-account</v-icon>
-      </v-btn>
-      <v-btn v-else text color="error">
-        Not Logged In
-        <v-icon>mdi-alert</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-navigation-drawer app expand-on-hover class="blue-grey lighten-4">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title text-uppercase">
+            Hinkskalle
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <template v-if="isLoggedIn">
+          <v-list-item link :to="'/account'">
+            <v-list-item-icon><v-icon>mdi-account</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{currentUser.fullname}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link :to="'/tokens'">
+            <v-list-item-icon><v-icon>mdi-lock-open</v-icon></v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Tokens</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+        </template>
+        <v-list-item link v-else color="error" :to="'/login'">
+          <v-list-item-icon><v-icon>mdi-alert</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Not Logged In</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item link :to="'/'">
+          <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="'/collections'">
+        <v-list-item-icon><v-icon>mdi-view-list</v-icon></v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Collections</v-list-item-title>
+        </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view></router-view>

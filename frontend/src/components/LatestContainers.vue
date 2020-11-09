e6fc3dc3bcdd95b36bce8c031891425eb68f9183
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-09 16:10:35 +0100

    latest container list

--- frontend/src/components/LatestContainers.vue
+++ frontend/src/components/LatestContainers.vue
@@ -1,12 +1,51 @@
 <template>
   <v-container>
-    oink
+    <v-row justify="center">
+          <v-col cols="6">
+            <h3>Latest Uploads:</h3>
+            <v-list nav>
+              <v-list-item-group>
+                <v-list-item link v-for="(up, i) in latest" :key="i"
+                    :to="{name: 'singularityDetail', params: { entity: up.container.entityName, collection: up.container.collectionName, container: up.container.name }}">
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{up.container.entityName}}/{{up.container.collectionName}}/{{up.container.name}}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                      <v-chip color="light-green lighten-2" v-for="tag in up.tags" :key="tag" @click.stop.prevent="copyTag(up.container, tag)">{{tag}}</v-chip>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+    </v-row>
   </v-container>
 </template>
 <script lang="ts">
 import Vue from 'vue';
 
+import { Container } from '../store/models';
+
 export default Vue.extend({
   name: 'LatestContainers',
+  mounted() {
+    this.loadLatest();
+  },
+  computed: {
+    latest() {
+      return this.$store.getters['containers/latest'];
+    },
+  },
+  methods: {
+    loadLatest() {
+      this.$store.dispatch('containers/latest');
+    },
+    copyTag: function(container: Container, tag: string) {
+      this.$copyText(`library://${container.entityName}/${container.collectionName}/${container.name}:${tag}`)
+        .then(() => this.$store.commit('snackbar/open', "Copied to clipboard."))
+    },
+  },
+
 });
 </script>
--- frontend/tests/unit/latest-containers.spec.ts
+++ frontend/tests/unit/latest-containers.spec.ts
@@ -1,20 +1,47 @@
 import { mount } from '@vue/test-utils';
 
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 
 import LatestContainers from '@/components/LatestContainers.vue';
 
 import { localVue } from '../setup';
+import { testLatestObj } from './store.spec';
+import { Upload } from '@/store/models';
 
 describe('LatestContainers.vue', () => {
   let vuetify: any;
+  let store: any;
+  let getters: any;
+  let actions: any;
+  let router: any;
+  let latest: Upload[] = [];
+  let mockLoadLatest: any;
 
   beforeEach(() => {
     vuetify = new Vuetify();
+    router = new VueRouter();
+    mockLoadLatest = jest.fn();
+    getters = {
+      'containers/latest': () => latest,
+    };
+    actions = {
+      'containers/latest': mockLoadLatest,
+    };
+    store = new Vuex.Store({ getters, actions });
   });
 
   it('renders something', () => {
-    const wrapper = mount(LatestContainers, { localVue, vuetify });
-    expect(wrapper.text()).toContain('oink');
+    const wrapper = mount(LatestContainers, { localVue, vuetify, store, router });
+    expect(wrapper.text()).toContain('Latest Uploads:');
+    expect(mockLoadLatest).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders uploads', () => {
+    latest = testLatestObj;
+    const wrapper = mount(LatestContainers, { localVue, vuetify, store, router });
+    expect(wrapper.findAll('a.v-list-item')).toHaveLength(2);
+
   });
 });

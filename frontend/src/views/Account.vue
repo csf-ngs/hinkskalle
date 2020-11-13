5b629a2adcbcd7bfa788eb71db0fa655195aa82b
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-13 17:26:45 +0100

    store actions, delete + confirm

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -27,7 +27,23 @@
               <v-row>
                 <v-col cols="12">
                   <v-btn type="reset" color="warning" class="mr-4" @click.prevent="reset()">Reset</v-btn>
-                  <v-btn type="submit" color="success" class="mr-4" @click="update()">Save</v-btn>
+                  <v-btn type="submit" color="success" class="mr-4" @click.prevent="update()">Save</v-btn>
+                  <v-dialog v-model="localState.confirmDelete" persistent max-width="250px">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn id="delete" color="error" v-bind="attrs" v-on="on">Delete</v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title class="headline">Delete Account?</v-card-title>
+                      <v-card-text>
+                        Delete yourself and all your containers and all that you stand for?
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="success" text @click="localState.confirmDelete=false">Hold on!</v-btn>
+                        <v-btn id="do-delete" color="error" text @click="deleteAccount()">Sayonara!</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
                 </v-col>
               </v-row>
             </v-container>
@@ -45,6 +61,7 @@ import { clone as _clone } from 'lodash';
 
 interface State {
   editUser: User | null;
+  confirmDelete: boolean;
 }
 
 export default Vue.extend({
@@ -52,6 +69,7 @@ export default Vue.extend({
   data: (): { localState: State } => ({
     localState: {
       editUser: null,
+      confirmDelete: false,
     },
   }),
   mounted: function() {
@@ -60,6 +78,26 @@ export default Vue.extend({
   methods: {
     reset() {
       this.localState.editUser = _clone(this.$store.getters.currentUser);
+    },
+    update() {
+      this.$store.dispatch('users/update', this.localState.editUser)
+        .then(user => {
+          console.log(user);
+          this.$store.commit('setUser', user);
+          this.localState.editUser = _clone(this.$store.getters.currentUser);
+          this.$store.commit('snackbar/showSuccess', "Hooray!");
+        });
+    },
+    deleteAccount() {
+      this.$store.dispatch('users/delete', this.localState.editUser)
+        .then(() => {
+          this.$store.commit('logout');
+          this.$router.push('/login');
+        })
+        .catch(err => {
+          this.$store.commit('snackbar/showError', err);
+        });
+      this.localState.confirmDelete=false;
     },
   }
 });
--- frontend/tests/unit/account.spec.ts
+++ frontend/tests/unit/account.spec.ts
@@ -1,25 +1,46 @@
 import { mount } from '@vue/test-utils';
 import Vuetify from 'vuetify';
 import Vuex from 'vuex';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 import Account from '@/views/Account.vue';
 
 import { localVue } from '../setup';
 
 import { testUserObj } from './store.spec';
+import {User} from '@/store/models';
 
 describe('Account.vue', () => {
   let vuetify: any;
   let store: any;
+  let router: VueRouter;
 
   let getters: any;
+  let actions: any;
+  let mutations: any;
+  let mockUpdateUser: jest.Mock<any, any>;
+  let mockDeleteUser: jest.Mock<any, any>;
 
   beforeEach(() => {
     vuetify = new Vuetify();
+    router = new VueRouter();
+    mockUpdateUser = jest.fn();
+    mockDeleteUser = jest.fn();
     getters = {
       currentUser: () => testUserObj,
     };
-    store = new Vuex.Store({ getters });
+    mutations = {
+      setUser: jest.fn(),
+      logout: jest.fn(),
+      'snackbar/showSuccess': jest.fn(),
+    };
+    actions = {
+      'users/update': mockUpdateUser,
+      'users/delete': mockDeleteUser,
+    };
+    store = new Vuex.Store({ getters, actions, mutations });
+    router = new VueRouter();
 
   });
 
@@ -50,5 +71,35 @@ describe('Account.vue', () => {
       expect(wrapper.vm.$data.localState.editUser.firstname).toBe(testUserObj.firstname);
       done();
     });
+  });
+
+  it('saves user', async done => {
+    const wrapper = mount(Account, { localVue, store, vuetify });
+    await Vue.nextTick();
+
+    wrapper.find("input#firstname").setValue('Oink');
+    wrapper.find('button[type="submit"]').trigger('click');
+    mockUpdateUser.mockResolvedValue(wrapper.vm.$data.localState.editUser);
+    expect(mockUpdateUser).toHaveBeenCalledWith(expect.anything(), wrapper.vm.$data.localState.editUser);
+
+    await Vue.nextTick();
+    await Vue.nextTick();
+    expect(mutations['snackbar/showSuccess']).toBeCalled();
+    done();
+  });
+
+  it('deletes user', async (done) => {
+    const wrapper = mount(Account, { localVue, store, vuetify, router });
+    await Vue.nextTick();
+    wrapper.find('button#delete').trigger('click');
+    await Vue.nextTick();
+    expect(wrapper.vm.$data.localState.confirmDelete).toBeTruthy();
+    wrapper.find('button#do-delete').trigger('click');
+    expect(mockDeleteUser).toBeCalledWith(expect.anything(), wrapper.vm.$data.localState.editUser);
+    await Vue.nextTick();
+    await Vue.nextTick();
+    expect(mutations.logout).toBeCalled();
+    
+    done();
   });
 });

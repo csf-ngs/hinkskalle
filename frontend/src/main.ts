7d331f25f596fdded71a05f4adf38a6c7b159c90
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-19 21:07:31 +0100

    prettyBytes filter

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,6 +20,29 @@ Vue.filter('abbreviate', function(value: string, maxlen: number): string {
 Vue.filter('pluralize', function(value: number, word: string): string {
   return value === 1 ? word : `${word}s`;
 });
+Vue.filter('prettyBytes', function (num: number): string {
+  // jacked from: https://github.com/sindresorhus/pretty-bytes
+  if (typeof num !== 'number' || isNaN(num)) {
+    throw new TypeError('Expected a number');
+  }
+
+  const neg = num < 0;
+  const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  if (neg) {
+    num = -num;
+  }
+
+  if (num < 1) {
+    return (neg ? '-' : '') + num + ' B';
+  }
+
+  const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
+  const outNum = (num / Math.pow(1000, exponent)).toFixed(2);
+  const unit = units[exponent];
+
+  return `${neg ? '-' : ''}${outNum} ${unit}`;
+});
 
 new Vue({
   router,

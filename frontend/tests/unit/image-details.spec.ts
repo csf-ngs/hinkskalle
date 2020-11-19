6552047a5f88020e553309688b491e10351fff93
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-11-19 22:13:53 +0100

    fix capitals

--- frontend/tests/unit/image-details.spec.ts
+++ frontend/tests/unit/image-details.spec.ts
@@ -5,7 +5,7 @@ import Vuetify from 'vuetify';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 
-import imageDetails from '@/components/imageDetails.vue';
+import ImageDetails from '@/components/ImageDetails.vue';
 
 import { localVue } from '../setup';
 import { testImagesObj } from './images-store.spec';
@@ -22,7 +22,7 @@ describe('LatestContainers.vue', () => {
   });
 
   it('renders something', () => {
-    const wrapper = mount(imageDetails, { localVue, vuetify, store, router });
+    const wrapper = mount(ImageDetails, { localVue, vuetify, store, router });
     wrapper.setProps({ image: testImagesObj[0] });
     expect(wrapper.text()).toBe('');
   });

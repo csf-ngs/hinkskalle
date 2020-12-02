2f56e5ae215b4372a9c662c616c66950dc0430ef
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-12-02 10:30:04 +0100

    user update/delete

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,13 +1,284 @@
 <template>
   <div class="users">
     <top-bar title="Users"></top-bar>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="10" offset-md="1">
+          <v-data-table
+            id="users"
+            :headers="headers"
+            :items="users"
+            :search="localState.search"
+            :sort-by="localState.sortBy"
+            :sort-desc="localState.sortDesc"
+            :loading="loading">
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-text-field 
+                  v-model="localState.search" 
+                  prepend-inner-icon="mdi-magnify" 
+                  label="Search..." 
+                  single-line outlined dense hide-details></v-text-field>
+                <v-spacer></v-spacer>
+                <v-dialog v-model="localState.showEdit" max-width="700px">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      dense depressed
+                      v-bind="attrs" v-on="on">Create User</v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title class="headline">{{editTitle}}</v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <hsk-text-input
+                              id="username"
+                              label="Username"
+                              field="username"
+                              :obj="localState.editItem"
+                              :readonly="!!localState.editItem.id"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <hsk-text-input
+                              id="firstname"
+                              label="Firstname"
+                              field="firstname"
+                              :obj="localState.editItem"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <hsk-text-input
+                              id="lastname"
+                              label="Lastname"
+                              field="lastname"
+                              :obj="localState.editItem"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                          <v-col cols="12">
+                            <hsk-text-input
+                              id="email"
+                              label="Email"
+                              field="email"
+                              :obj="localState.editItem"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <v-text-field
+                              :type="localState.passwordVisible ? 'text' : 'password'"
+                              outlined
+                              hide-details="auto"
+                              v-model="localState.password1"
+                              label="Password"
+                              :error="!passwordsMatching"
+                              :append-icon="localState.passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+                              @click:append="localState.passwordVisible=!localState.passwordVisible"
+                              ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <v-text-field
+                              :type="localState.passwordVisible ? 'text' : 'password'"
+                              outlined
+                              hide-details="auto"
+                              v-model="localState.password2"
+                              label="Repeat"
+                              :error="!passwordsMatching"
+                              :append-icon="localState.passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+                              @click:append="localState.passwordVisible=!localState.passwordVisible"
+                              ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <hsk-text-input 
+                              type="yesno"
+                              label="Active?"
+                              field="isActive"
+                              :obj="localState.editItem"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                          <v-col cols="12" md="6">
+                            <hsk-text-input 
+                              type="yesno"
+                              label="Admin?"
+                              field="isAdmin"
+                              :obj="localState.editItem"
+                              @updated="localState.editItem=$event"></hsk-text-input>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="secondary accent-1" text @click="closeEdit">Mabye not today.</v-btn>
+                      <v-btn color="primary darken-1" text @click="save">Save It!</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-dialog v-model="localState.showDelete" max-width="500px">
+                  <v-card>
+                    <v-card-title class="headline">You really want to dispose of that?</v-card-title>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="secondary darken-1" text @click="closeDelete">Let mercy rule.</v-btn>
+                      <v-btn color="warning accent-1" text @click="deleteConfirm">Get it out of my sight.</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+                <v-btn 
+                  id="refresh" 
+                  class="ml-2"
+                  dense depressed 
+                  @click="loadUsers()"><v-icon>mdi-refresh</v-icon></v-btn>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.lastname="{ item }">
+              {{item.firstname}} {{item.lastname}}
+            </template>
+            <template v-slot:item.isActive="{ item }">
+              <v-icon>{{item.isActive ? 'mdi-check' : 'mdi-minus'}}</v-icon>
+            </template>
+            <template v-slot:item.isAdmin="{ item }">
+              <v-icon>{{item.isAdmin ? 'mdi-wizard-hat' : 'mdi-minus'}}</v-icon>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                small
+                class="mr-2"
+                @click="editUser(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon
+                small
+                @click="deleteUser(item)">
+                mdi-delete
+              </v-icon>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
   
 </template>
 <script lang="ts">
+import { User } from '@/store/models';
 import Vue from 'vue';
+import { DataTableHeader } from 'vuetify';
+import { cloneDeep as _cloneDeep } from 'lodash';
+
+interface State {
+  search: string;
+  sortBy: string;
+  sortDesc: boolean;
+  editItem: User;
+  showEdit: boolean;
+  showDelete: boolean;
+  passwordVisible: boolean;
+  password1: string;
+  password2: string;
+}
+
+function defaultItem(): User {
+  const user = new User();
+  user.createdAt = new Date();
+  user.isActive = true;
+  user.isAdmin = false;
+  return user;
+}
 
 export default Vue.extend({
   name: 'Users',
+  mounted() {
+    this.loadUsers();
+  },
+  data: (): { headers: DataTableHeader[]; localState: State } => ({
+    headers: [
+      { text: 'id', value: 'id', sortable: true, filterable: false, width: '5%' },
+      { text: 'Username', value: 'username', sortable: true, filterable: true, width: '15%' },
+      { text: 'Email', value: 'email', sortable: true, filterable: true, width: '15%' },
+      { text: 'Name', value: 'lastname', sortable: true, filterable: true, width: '', },
+      { text: 'Admin', value: 'isAdmin', sortable: true, filterable: false, width: '5%' },
+      { text: 'Active', value: 'isActive', sortable: true, filterable: false, width: '5%' },
+      { text: 'Source', value: 'source', sortable: true, filterable: true, width: '9%' },
+      { text: 'Actions', value: 'actions', sortable: false, filterable: false, width: '1%' },
+    ],
+    localState: {
+      search: '',
+      sortBy: 'id',
+      sortDesc: false,
+      editItem: defaultItem(),
+      showEdit: false,
+      showDelete: false,
+      passwordVisible: false,
+      password1: '',
+      password2: '',
+    },
+  }),
+  computed: {
+    users() {
+      return this.$store.getters['users/list'];
+    },
+    loading() {
+      return this.$store.getters['tokens/status']==='loading';
+    },
+    editTitle(): string {
+      return this.localState.editItem.id ? 'Edit User' : 'Create User';
+    },
+    passwordsMatching(): boolean {
+      return this.localState.password1 === this.localState.password2;
+    }
+  },
+  watch: {
+    'localState.showEdit': function showEdit(val) {
+      if (!val) {
+        this.closeEdit();
+      }
+    },
+    'localState.showDelete': function showDelete(val) {
+      if (!val) {
+        this.closeDelete();
+      }
+    },
+  },
+  methods: {
+    loadUsers() {
+      this.$store.dispatch('users/list')
+        .catch(err => this.$store.commit('snackbar/showError', err));
+    },
+    closeEdit() {
+      this.localState.showEdit = false;
+      this.$nextTick(() => {
+        this.localState.editItem = defaultItem();
+      });
+    },
+    editUser(user: User) {
+      this.localState.editItem = _cloneDeep(user);
+      this.localState.showEdit = true;
+    },
+    deleteUser(user: User) {
+      this.localState.editItem = _cloneDeep(user);
+      this.localState.showDelete = true;
+    },
+    closeDelete() {
+      this.localState.showDelete = false;
+      this.$nextTick(() => {
+        this.localState.editItem = defaultItem();
+      });
+    },
+    save() {
+      const action = this.localState.editItem.id ?
+        'users/update' : 'users/create';
+      this.$store.dispatch(action, this.localState.editItem)
+        .then(() => this.$store.commit('snackbar/showSuccess', 'Hoorary!'))
+        .catch(err => this.$store.commit('snackbar/showError', err));
+      this.closeEdit();
+    },
+    deleteConfirm() {
+      this.$store.dispatch('users/delete', this.localState.editItem)
+        .then(() => this.$store.commit('snackbar/showSuccess', "It's gone!"))
+        .catch(err => this.$store.commit('snackbar/showError', err));
+      this.closeDelete();
+    },
+  },
 });
 </script>
--- frontend/tests/unit/users.spec.ts
+++ frontend/tests/unit/users.spec.ts
@@ -8,27 +8,51 @@ import VueRouter from 'vue-router';
 import { localVue } from '../setup';
 
 import Users from '@/views/Users.vue';
+import { makeTestUserObj } from '../_data';
 
 describe('Users.vue', () => {
   let vuetify: any;
   let store: any;
   let router: VueRouter;
 
-  let getters: any;
-  let actions: any;
+  let getters: { [name: string]: jest.Mock<any, any>};
+  let actions: { [name: string]: jest.Mock<any, any>};
 
   beforeEach(() => {
     vuetify = new Vuetify();
     router = new VueRouter({});
 
-    getters = {};
-    actions = {};
+    getters = {
+      'users/list': jest.fn(),
+    };
+    actions = {
+      'users/list': jest.fn(),
+    };
     store = new Vuex.Store({ getters, actions });
   });
 
   it('renders something', () => {
     const wrapper = mount(Users, { localVue, vuetify, store, router });
     expect(wrapper.text()).toContain('Users');
+    expect(actions['users/list']).toHaveBeenCalled();
+  });
+
+  it('renders user rows', () => {
+    const testUsers = [ makeTestUserObj(), makeTestUserObj() ];
+    testUsers[1].id="2";
+    getters['users/list'].mockReturnValue(testUsers);
+    const wrapper = mount(Users, { localVue, vuetify, store, router });
+    expect(wrapper.findAll('div#users tbody tr')).toHaveLength(2);
+  });
+
+  it('checks password matching', ()=> {
+    const wrapper = mount(Users, { localVue, vuetify, store, router });
+    expect((wrapper.vm as any).passwordsMatching).toBeTruthy();
+    wrapper.setData({ localState: { password1: 'eins', password2: 'eins' } });
+    expect((wrapper.vm as any).passwordsMatching).toBeTruthy();
+    wrapper.setData({ localState: { password1: 'eins', password2: 'zwei' } });
+    expect((wrapper.vm as any).passwordsMatching).toBeFalsy();
+
   });
 
 });

483a9b3b1777fbd616aff66b05fcb8bcf33036dd
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2020-12-21 22:54:52 +0100

    record some housekeeping

--- backend/Hinkskalle/util/jobs.py
+++ backend/Hinkskalle/util/jobs.py
@@ -7,6 +7,7 @@ from time import sleep
 from rq.job import Job
 from .auth.ldap import LDAPUsers, _get_attr
 from .auth.exceptions import UserConflict
+from datetime import datetime
 
 rq = RQ()
 
@@ -19,12 +20,16 @@ def sync_ldap():
   job = get_current_job()
   svc = LDAPUsers(app=current_app)
   svc.ldap.connect()
-  ldap_users = svc.ldap.list_users()
+
   result = {
+    'job': job.id,
+    'started': datetime.now().isoformat(),
     'synced': [],
     'conflict': [],
     'failed': [],
   }
+
+  ldap_users = svc.ldap.list_users()
   for ldap_user in ldap_users:
     try:
       db_user = svc.sync_user(ldap_user)
@@ -34,6 +39,7 @@ def sync_ldap():
     except:
       result['failed'].append(_get_attr(ldap_user.get('attributes').get('cn')))
 
+  result['finished']=datetime.now().isoformat()
   job.meta['result']=result
   job.save_meta()
 

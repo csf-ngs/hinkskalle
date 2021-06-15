58690dae6e8de869b1adcdd9d363c53a4f44024c
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2021-06-15 23:42:52 +0200

    i want to use any

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-explicit-any': 0,
+    '@typescript-eslint/no-explicit-any': "off",
+    '@typescript-eslint/camelcase': ["error", {allow: ["total_size"]}],
   },
   overrides: [
     {
@@ -24,7 +25,7 @@ module.exports = {
       ],
       env: {
         jest: true
-      }
+      },
     }
   ]
 }

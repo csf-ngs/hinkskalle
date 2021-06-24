6f02f94eb741e4d962a7bba00b057fdd632b718c
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2021-06-24 13:21:34 +0200

    Bump version: 4.2.1 → 4.2.2

--- backend/setup.py
+++ backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Hinkskalle',
-    version='4.2.1',
+    version='4.2.2',
     packages=['Hinkskalle'],
     include_package_data=True,
     install_requires=[

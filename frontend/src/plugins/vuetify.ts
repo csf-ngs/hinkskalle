37ca6b8c6ee3530fdefe3bf627d5dd1556886105
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2021-07-17 23:00:46 +0200

    inline/self-host fonts

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
+import '@mdi/font/css/materialdesignicons.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({

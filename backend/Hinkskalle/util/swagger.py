babe680989d6f7d312a83f9b4c813d75d86af959
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2022-04-05 21:27:39 +0200

    use new style auth converters

--- backend/Hinkskalle/util/swagger.py
+++ backend/Hinkskalle/util/swagger.py
@@ -1,23 +1,32 @@
 from flask_rebar.swagger_generation import swagger_words as sw
-from flask_rebar.swagger_generation.authenticator_to_swagger import AuthenticatorConverterRegistry
-from flask_rebar.swagger_generation.authenticator_to_swagger import make_class_from_method
+from flask_rebar.swagger_generation.authenticator_to_swagger import AuthenticatorConverterRegistry, AuthenticatorConverter
+#from flask_rebar.swagger_generation.authenticator_to_swagger import make_class_from_method
+
 from flask_rebar.swagger_generation.marshmallow_to_swagger import NestedConverter, request_body_converter_registry, response_converter_registry
 
 from .auth.token import TokenAuthenticator, ScopedTokenAuthenticator
 
+class TokenAuthConverter(AuthenticatorConverter):
+    AUTHENTICATOR_TYPE=TokenAuthenticator
+    def get_security_schemes(self, obj, context=None):
+        return {
+            obj.name: {sw.type_: sw.api_key, sw.in_: TokenAuthenticator.header, sw.name: TokenAuthenticator.type}
+        }
+    def get_security_requirements(self, obj, context=None):
+        return [{obj.name: []}]
+
+class ScopedTokenAuthConverter(AuthenticatorConverter):
+    AUTHENTICATOR_TYPE=ScopedTokenAuthenticator
+    def get_security_schemes(self, obj, context=None):
+        return {
+            'token_auth': {sw.type_: sw.api_key, sw.in_: TokenAuthenticator.header, sw.name: TokenAuthenticator.type}
+        }
+    def get_security_requirements(self, obj, context=None):
+        return [{'token_auth': []}]
+
 def register_authenticators(registry: AuthenticatorConverterRegistry):
-  registry.register_type(auth_converter)
-  registry.register_type(scoped_auth_converter)
-
-def _convert_authenticator(authenticator):
-  definition = { sw.name: authenticator.type, sw.in_: authenticator.header, sw.type_: sw.api_key }
-  return 'token_auth', definition
-
-def _convert_scoped_authenticator(authenticator):
-  return _convert_authenticator(authenticator.authenticator)
-
-auth_converter = make_class_from_method(TokenAuthenticator, _convert_authenticator)
-scoped_auth_converter = make_class_from_method(ScopedTokenAuthenticator, _convert_scoped_authenticator)
+  registry.register_type(TokenAuthConverter())
+  registry.register_type(ScopedTokenAuthConverter())
 
 # see https://github.com/plangrid/flask-rebar/issues/90
 class MyNestedConverter(NestedConverter):

ceab8a43079e20e4e9d42d825601310439c6d618
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2022-05-04 23:01:53 +0200

    minimal deploy for conformance tests

--- share/oci/conformance-test.sh
+++ share/oci/conformance-test.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-pushd ../../
-docker-compose exec api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
-popd
+mkdir -p conf/
+echo "POSTGRES_PASSWORD=supersecret" > conf/db_secrets.env
+echo "HINKSKALLE_SECRET_KEY=superdupersecret" > conf/secrets.env
+echo "DB_PASSWORD=supersecret" >> conf/secrets.env
+
+docker-compose --project-directory ../../ up -d
+sleep 5
+docker-compose --project-directory ../../ exec api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
+echo "Starting tests..."
 docker run --rm \
   --network host \
   -v $(pwd)/results:/results \
   -w /results \
-  -e OCI_ROOT_URL="http://127.0.0.1:7660" \
+  -e OCI_ROOT_URL="http://${BACKEND_HOST:-127.0.0.1}:${BACKEND_PORT:-17660}" \
   -e OCI_NAMESPACE="myorg/myrepo" \
   -e OCI_USERNAME="conform.hase" \
   -e OCI_PASSWORD="conform" \
@@ -20,6 +26,6 @@ docker run --rm \
   -e OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 \
   ghcr.io/csf-ngs/oci-conformance
 
-pushd ../../
-docker-compose exec api flask localdb remove-user conform.hase
-popd
+docker-compose --project-directory ../../ exec api flask localdb remove-user conform.hase
+docker-compose --project-directory ../../ logs api > results/api.log
+docker-compose --project-directory ../../ down

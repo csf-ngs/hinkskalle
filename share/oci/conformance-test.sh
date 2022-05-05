73089f43c6939052c409be11b3d2b78b347ce050
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2022-05-05 21:52:03 +0200

    no tty in drone

--- share/oci/conformance-test.sh
+++ share/oci/conformance-test.sh
@@ -11,7 +11,7 @@ set -eo pipefail
 docker-compose --project-directory ../../ config
 docker-compose --project-directory ../../ up -d
 sleep 1
-docker-compose --project-directory ../../ exec api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
+docker-compose --project-directory ../../ exec -T api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
 set +eo pipefail
 echo "Starting tests..."
 docker run --rm \
@@ -31,6 +31,5 @@ docker run --rm \
   -e OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 \
   ghcr.io/csf-ngs/oci-conformance
 
-docker-compose --project-directory ../../ exec api flask localdb remove-user conform.hase
 docker-compose --project-directory ../../ logs api > results/api.log
 docker-compose --project-directory ../../ down

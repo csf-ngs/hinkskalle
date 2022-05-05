6cd5428f9048422e32cd7a38bb013f338a57a51c
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2022-05-05 22:22:21 +0200

    fix env default, set project name

--- share/oci/conformance-test.sh
+++ share/oci/conformance-test.sh
@@ -8,17 +8,17 @@ echo "HINKSKALLE_SECRET_KEY=superdupersecret" > conf/secrets.env
 echo "DB_PASSWORD=supersecret" >> conf/secrets.env
 
 set -eo pipefail
-docker-compose --project-directory ../../ config
-docker-compose --project-directory ../../ up -d
+docker-compose -p conformance --project-directory ../../ config
+docker-compose -p conformance --project-directory ../../ up -d
 sleep 1
-docker-compose --project-directory ../../ exec -T api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
+docker-compose -p conformance --project-directory ../../ exec -T api flask localdb add-user -u conform.hase -p conform -e conform@testha.se -f Conform -l Hase --admin
 set +eo pipefail
 echo "Starting tests..."
 docker run --rm \
   --add-host=host.docker.internal:host-gateway \
   -v $(pwd)/results:/results \
   -w /results \
-  -e OCI_ROOT_URL="http://${BACKEND_HOST:-host.docker.internal}:${BACKEND_PORT:-17660}" \
+  -e OCI_ROOT_URL="http://${BACKEND_HOST:-host.docker.internal}:${BACKEND_PORT:-7660}" \
   -e OCI_NAMESPACE="myorg/myrepo" \
   -e OCI_USERNAME="conform.hase" \
   -e OCI_PASSWORD="conform" \
@@ -31,5 +31,5 @@ docker run --rm \
   -e OCI_DELETE_MANIFEST_BEFORE_BLOBS=1 \
   ghcr.io/csf-ngs/oci-conformance
 
-docker-compose --project-directory ../../ logs api > results/api.log
-docker-compose --project-directory ../../ down
+docker-compose -p conformance --project-directory ../../ logs api > results/api.log
+docker-compose -p conformance --project-directory ../../ down
